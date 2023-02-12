75c865ffebb8688781fc9f6e8ad978f3563468da
Shedow321 <Chernij.illga@gmail.com>
2023-02-12 11:51:02 +0200

    My first ruby file

---
+++ Lesson1.rb
@@ -0,0 +1,16 @@
+name= "Людина Павук"
+superPower= "Липкі руки"
+age=27
+puts name
+puts age
+desc= "Герой: #{name}. Він має: #{superPower}. йому #{age}"
+puts desc
+
+print"HI, Ruby"
+print"HI, Ruby"
+puts name
+
+puts name.upcase
+puts name.downcase
+puts superPower.reverse
+puts superPower.reverse.upcase
